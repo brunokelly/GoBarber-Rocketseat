316fdd2a6d034e79f1050d0b66947ca40ef0708d
Bruno Kelly <brunobuch00@gmail.com>
2021-04-21 19:48:46 -0300

    validacao de data

--- src/routes/appointments.route.ts
+++ src/routes/appointments.route.ts
@@ -1,22 +1,41 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmetnsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmetnsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
     
+    const parsedDate = startOfHour(parseISO(date));
+    
+    const findAppointmentInSameDate = appointments.find(appointment => 
+        isEqual(parsedDate, appointment.date)
+    );
+
+    if(findAppointmentInSameDate) {
+        return response 
+            .status(400)
+            .json({message: 'Horario ja ocupado'});
+    }
+
     const appointment = {
         id: uuid(),
         provider,
-        date,
+        date: parsedDate,
     };
 
     appointments.push(appointment);
 
-    return response.json({message: 'Hello'})
+    return response.json(appointment);
 });
 
 export default appointmetnsRouter;
