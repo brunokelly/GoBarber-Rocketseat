b4f1148d472aba3a327833f0a7a1a072de7d6539
Bruno Kelly <brunobuch00@gmail.com>
2021-04-21 21:33:36 -0300

    Listando agendamentos e aplicando conceito SOLID

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,29 +1,44 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
+//#region INTERFACE
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+//#endregion
+
 class AppointmentsRepository {
-    private appointments: Appointment[];
-    
-    constructor(){
-        this.appointments = [];
+  private appointments: Appointment[];
 
-    }
+  constructor() {
+    this.appointments = [];
+  }
 
-    public findByDate(date: Date): Appointment | null {
-        const findAppointment= this.appointments.find(appointment => 
-            isEqual(date, appointment.date)
-            );
-    
-        return findAppointment || null;
-    }
+  //#region METODOS
 
-    public create(provider: string, date: Date): Appointment {
-        const appointment = new Appointment(provider, date);
+  public all(): Appointment[] {
+    return this.appointments;
+  }
 
-        this.appointments.push(appointment)
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
 
-        return appointment
-    } 
+    return findAppointment || null;
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  //#endregion
 }
 
 export default AppointmentsRepository;
--- src/routes/appointments.route.ts
+++ src/routes/appointments.route.ts
@@ -1,29 +1,37 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../service/CreateAppointmentService';
 
 const appointmetnsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmetnsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmetnsRouter.post('/', (request, response) => {
+  try {
     const { provider, date } = request.body;
-    
-    const parsedDate = startOfHour(parseISO(date));
-    
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(
-        parsedDate,
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
     );
 
-    if(findAppointmentInSameDate) {
-        return response 
-            .status(400)
-            .json({message: 'Horario ja ocupado'});
-    }
-
-    const appointment = appointmentsRepository.create(provider, parsedDate);
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
 
     return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default appointmetnsRouter;
