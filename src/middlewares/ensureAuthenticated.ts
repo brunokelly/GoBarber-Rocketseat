0e39e77173386fb72e2fd3b23650f09a357b9798
Bruno Kelly <brunobuch00@gmail.com>
2021-06-03 12:27:19 -0300

    upload de imagens

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -3,13 +3,18 @@ import { verify } from 'jsonwebtoken';
 
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction,
 ): void {
   //Validacao do token JWT
-
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
@@ -20,6 +25,12 @@ export default function ensureAuthenticated(
 
   try {
     const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch {
