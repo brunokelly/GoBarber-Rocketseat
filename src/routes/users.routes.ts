865cf8ddd324c5865e4c5a3b8a40425d1f93ceae
Bruno Kelly <brunobuch00@gmail.com>
2021-06-03 17:11:55 -0300

    atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,8 +3,9 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../service/CreateUserService';
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
@@ -33,7 +34,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
